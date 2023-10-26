2ddb8194da94a840231e23a42d5776107a76b679
Mahipal Singh <mahipalsinghrao0938@gmail.com>
2023-10-27 02:18:06 +0530

    provide backend support

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === 'production' ? 'https://drawsketch-server.netlify.app/' : 'http://localhost:5000'
+const URL = process.env.NODE_ENV === 'production' ? 'https://drawingboard-server.onrender.com' : 'http://localhost:5000'
 export const socket = io(URL);
